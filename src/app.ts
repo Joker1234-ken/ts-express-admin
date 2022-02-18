669f4560b484665524f6734953aa01f7830ae9eb
Joker123-ken <862175189@qq.com>
2022-02-18 12:01:41 +0800

    feat: 解析express request body参数

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import express, { Express } from 'express'
 
 const app: Express = express()
 
-app.use((req, res) => {
-  res.send('welcome to my node server')
-})
+// 解析express request body参数
+app.use(express.json(), express.urlencoded({ extended: false }))
 
 app.listen(8088, () => {
   console.log('http://localhost:8088')
