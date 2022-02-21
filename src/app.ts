26583d5d6ce8d5e6aa78f293d072e757c174a643
Joker123-ken <862175189@qq.com>
2022-02-22 00:28:28 +0800

    feat: 入口文件 更新

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { setupRouter } from './router/index'
+import { setupRouter } from './controllers/index'
 import express, { Express } from 'express'
 
 const app: Express = express()
@@ -6,6 +6,7 @@ const app: Express = express()
 // 解析express request body参数
 app.use(express.json(), express.urlencoded({ extended: false }))
 
+// 路由控制器
 setupRouter(app)
 
 app.listen(8088, () => {
