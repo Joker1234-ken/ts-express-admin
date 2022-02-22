041d3a991fddf0b6a03b5c400caeb31c94e8bfdb
hanaikun <862175189@qq.com>
2022-02-22 18:34:08 +0800

    feat: error全局定义

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import { setupRouter } from './controllers/index'
+import { setupRouter } from './router/index'
 import express, { Express } from 'express'
+import { setupError } from './error/index'
 
 const app: Express = express()
 
@@ -8,6 +9,9 @@ app.use(express.json(), express.urlencoded({ extended: false }))
 
 // 路由控制器
 setupRouter(app)
+
+// 错误处理
+setupError(app)
 
 app.listen(8088, () => {
   console.log('http://localhost:8088')
